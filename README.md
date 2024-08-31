72d22f2709d2ef9015b6c8e361bbc72a8abcd0ee
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-08-31 18:00:24 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 #stravaProj
+
+
+step 1 : get cleint id and cleint secret
+
+client id = 111
+client secret = 111
+
+
+step 2 : get authorization code
+
+on clicking a button this url must open in browser :  http://www.strava.com/oauth/authorize?client_id=111&redirect_uri=http://localhost&response_type=code&scope=activity:read_all
+
+
+https://www.strava.com/oauth/authorize?client_id=100329&redirect_uri=http://localhost&response_type=code&scope=activity:read_all
+
+
+Upon succesfull login, this url opens : http://localhost/?state=&code=1110&scope=read,activity:read_all  store code into a variable
+
+
+next post this request : https://www.strava.com/oauth/token?client_id=100329&client_secret=111&code=1110&grant_type=authorization_code to get refresh toekn and access token in repsonse
+
+
+
+
+step 4 :
+
+https://www.strava.com/api/v3/athlete/activities?access_token=5fe0cb60f7b0974bb7de3dcb38403b121fc51132
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+const Home = () => {
+  const clientId = import.meta.env.VITE_API_CLIENT_ID;
+    const clientSecret = import.meta.env.VITE_API_CLIENT_SECRET;
+    const [authCode, setAuthCode] = useState(null);
+    const [accessToken, setAccessToken] = useState(null);
+
+  const redirectUri = "http://localhost:5173";
+  const responseType = "code";
+  const scope = "activity:read_all";
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    if (code) {
+      setAuthCode(code);
+      console.log("Authorization Code:", code);
+      getToken(code);
+    }
+  }, []);
+
+  const handleAuth = () => {
+    const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+    window.location.href = authUrl; // Redirect to Strava's authorization page
+  };
+
+  const getToken = async (code) => {
+    const tokenUrl = "https://www.strava.com/oauth/token";
+    const response = await fetch(tokenUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code,
+        grant_type: "authorization_code",
+      }),
+    });
+
+    const data = await response.json();
+    console.log("Access Token:", data.access_token);
+    setAccessToken(data.access_token);
+  };
+
+  return (
+    <div>
+      <h1>Strava OAuth Demo</h1>
+      <button onClick={handleAuth}>Connect with Strava</button>
+      {authCode && <p>Authorization Code: {authCode}</p>}
+      {accessToken && <p>Access Token: {accessToken}</p>}
+    </div>
+  );
+};
+
+export default Home;
